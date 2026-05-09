86cdb986d628aa790086e67f6ffb617a0d0fcc93
admin <bugsteam9912@gmail.com>
2026-05-09 21:59:27 +0900

    [add] Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,81 @@
-﻿
+﻿using CSharpProductPractice.DTOs;
+using CSharpProductPractice.Models;
+using CSharpProductPractice.Repositories;
+using CSharpProductPractice.Services;
+
+IProductRepository productRepository = new ProductRepository();
+IProductService productService = new ProductService(productRepository);
+
+try
+{
+    ProductResponse product1 = productService.CreateProduct(
+        new ProuductCreateRequest
+        {
+            Name = "노트북",
+            Price = 1200000,
+            StockQty = 10
+        }
+    );
+
+    ProductResponse product2 = productService.CreateProduct(
+        new ProuductCreateRequest
+        {
+            Name = "키보드",
+            Price = 30000,
+            StockQty = 50
+        }
+    );
+
+    Console.WriteLine("=== 상품 등록 완료 ===");
+    PrintProduct(product1);
+    PrintProduct(product2);
+
+    Console.WriteLine();
+    Console.WriteLine("=== 전체 상품 목록 ===");
+
+    List<ProductResponse> products = productService.GetProducts();
+
+    foreach (ProductResponse product in products)
+    {
+        PrintProduct(product);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("=== 상품 수정 ===");
+
+    ProductResponse updatedProduct = productService.UpdateProduct(
+        product1.Id,
+        new ProductUpdateRequest
+        {
+            Name = "게이밍 노트북",
+            Price = 1500000,
+            StockQty = 7
+        }
+    );
+
+    PrintProduct(updatedProduct);
+
+    Console.WriteLine();
+    Console.WriteLine("=== 상품 삭제 ===");
+
+    productService.DeleteProduct(product2.Id);
+
+    Console.WriteLine();
+    Console.WriteLine("=== 삭제 후 상품 목록 ===");
+
+    foreach (ProductResponse product in productService.GetProducts())
+    {
+        PrintProduct(product);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"오류 발생 : {e.Message}");
+}
+
+void PrintProduct(ProductResponse product)
+{
+    Console.WriteLine(
+        $"ID: {product.Id}, 이름: {product.Name}, 가격: {product.Price}, 재고 : {product.StockQty}, 활성화 : {product.IsActive}"
+    );
+}
--- obj/Debug/net10.0/CSharpProductPractice.AssemblyInfo.cs
+++ obj/Debug/net10.0/CSharpProductPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpProductPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c933976f95e93d88e3bc2ce0bde0337ee8328e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpProductPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpProductPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
